83c659fb6983e66221120d99e97603c1e3b1eac2
SamStarWalker <mariezsamuel@gmail.com>
2026-05-11 21:44:01 -0400

    Update main.cpp

--- SFML_RPG/main.cpp
+++ SFML_RPG/main.cpp
@@ -1,25 +1,9 @@
-#include <iostream>
-#include <ctime>
-#include <cstdlib>
-
-#include <SFML/Graphics.hpp>
-
+#include "Game.h"
+ 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "C++ SFML RPG PROJECT");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+    Game game;
+    game.run();
+ 
+    return 0;
 }
