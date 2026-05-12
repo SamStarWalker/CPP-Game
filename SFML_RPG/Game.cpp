6176179e4c9b482e77dff9ae97d2185aac9fd8a5
SamStarWalker <mariezsamuel@gmail.com>
2026-05-11 21:43:18 -0400

    Update Game.cpp
    more code stuff

--- SFML_RPG/Game.cpp
+++ SFML_RPG/Game.cpp
@@ -1,11 +1,88 @@
 #include "Game.h"
+ 
+//Static Functions
+ 
+//Initializer Functions
 
+void Game::initWindow()
+
+{
+
+    /*creates a SFML window using options from window.init file*/
+
+    this->window = new sf::RenderWindow (sf::VideoMode({ 800, 600 }), "SWORD ART ONLINE");
+
+}
+ 
+//Contructors/Destructors
+ 
 Game::Game()
+
 {
 
-}
+    this->initWindow();
 
+}
+ 
 Game::~Game()
+
 {
 
+    delete this->window;
+
 }
+ 
+//Functions
+ 
+void Game::updateSFMLEvents()
+
+{
+
+    while (const std::optional event = this->window->pollEvent())
+
+    {
+
+        if (event->is<sf::Event::Closed>())
+
+            this->window->close();
+
+    }
+
+}
+ 
+void Game::update()
+
+{
+
+    this->updateSFMLEvents();
+
+}
+ 
+void Game::render()
+
+{
+
+    this->window->clear();
+ 
+    //Render Items
+ 
+    this->window->display();
+
+}
+ 
+void Game::run()
+
+{
+
+    while (this->window->isOpen())
+
+    {
+
+        this->update();
+
+        this->render();
+
+    }
+
+}
+ 
