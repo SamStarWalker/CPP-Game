62e71be6d10f32f6a2a35b3da288c7267b375069
SamStarWalker <mariezsamuel@gmail.com>
2026-05-11 21:43:43 -0400

    Update Game.h
    more codee

--- SFML_RPG/Game.h
+++ SFML_RPG/Game.h
@@ -1,13 +1,33 @@
 #ifndef GAME_H
 #define GAME_H
+ 
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+ 
 class Game
 {
 private:
-
-
+	//Variables
+private:
+	sf::RenderWindow* window;
+ 
+	//Initialization
+	void initWindow();
+ 
 public:
+	//Constructors/Destructors
 	Game();
 	virtual ~Game();
+	//Functions
+	void updateSFMLEvents();
+	void update();
+	void render();
+	void run();
 };
 #endif
-
